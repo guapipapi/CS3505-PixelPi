1e927c43bc882b80695b1cfd2045f10b353bf353
Roland Guerriere <rolandguerriere@proton.me>
2025-03-26 21:07:04 -0600

    added brush paint functionallity

--- brush.cpp
+++ brush.cpp
@@ -2,4 +2,17 @@
 
 Brush::Brush(QObject *parent)
     : QObject{parent}
+    , radius{1}
 {}
+
+int Brush::getRadius() {
+    return radius;
+}
+
+void Brush::setRadius(int radius) {
+    this->radius = radius;
+}
+
+void Brush::paintPixel(int x, int y, Pixel& pixel, Sprite& sprite) {
+    sprite.addPixel(x, y, pixel);
+}
--- brush.h
+++ brush.h
@@ -2,6 +2,7 @@
 #define BRUSH_H
 
 #include <QObject>
+#include "sprite.h"
 
 class Brush : public QObject
 {
@@ -9,8 +10,8 @@ class Brush : public QObject
 public:
     explicit Brush(QObject *parent = nullptr);
     int getRadius();
-    void increaseRadius();
-    void decreaseRadius();
+    void setRadius(int radius);
+    void paintPixel(int x, int y, Pixel& pixel, Sprite& sprite);
 private:
     int radius;
 
