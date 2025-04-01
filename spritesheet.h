67a38be49b9b26c069e047abe9d236e3e8f679c1
khalil-imran <khalil.a.imran@gmail.com>
2025-03-31 22:32:43 -0600

    Corrected comment formatting

--- spritesheet.h
+++ spritesheet.h
@@ -1,5 +1,5 @@
 /**
- * @Authors: ....
+ * @author: Carlos Alatorre
  *
  * The spritesheet class is a model that handles all related buttons
  * and activity from the user related to the use of the sprites.
@@ -19,22 +19,49 @@ class Spritesheet : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * Constructs the a QObject and current status of a palette
+     * @param parent - pointer to a parent QObject
+     */
     explicit Spritesheet(QObject *parent = nullptr);
-    // Returns true if operation was a success
+
+    /**
+     * This method saves the current sprite to a JSON for the user
+     * @param filePath - the current file path where the user wants to save
+     * @return - true if file was able to be saved, false otherwise
+     */
     bool saveToJson(QString& filePath);
 
-    // Returns true if operation was a success
+    /**
+     * This method loads a sprite from a JSON to the current sprite sheet
+     * @param filePath - the current file path the user is using to load sprite
+     * @return - true if the file was able to be loaded, false otherwise
+     */
     bool loadJson(QString& filePath);
 
-    // Returns the current sprite
+    /**
+     * This method returns the current sprite from the spritesheet
+     * @return - the current sprite
+     */
     Sprite& getCurrentSprite();
 
-    // Creates a new project
+    /**
+     * This method creates a new instance of a frame/sprite for the user to work on
+     * @param newWidth - the new width value
+     * @param newHeight - the new height value
+     */
     void newProject(int newWidth, int newHeight);
 
-    // Gets a reference of current palette
+    /**
+      * This method returns the current palette
+      * @return - the current palette
+      */
     Palette& getPalette();
 
+    /**
+     * Gets the current timeline of the spritesheet
+     * @return - the current timeline
+     */
     Timeline& getTimeline();
 
     //Convert sprite sheet to JSON
@@ -71,18 +98,43 @@ private:
     std::vector<Sprite> sprites;
 
 signals:
-    // Updates sprite to the spritesheet
+    /**
+     * This is a signal that updates the current sprite
+     * @param currentSprite - the current sprite to be updated
+     */
     void currentSpriteUpdated(Sprite* currentSprite);
     void currentSpriteID(int spriteID);
 
 public slots:
-    // Gets location of drawing the sprite
+    /**
+     * This method gets the current coordinates of where the
+     * current sprite is being painted
+     * @param x - the x coordinate
+     * @param y - the y coordinate
+     */
     void paintedCurrentSpriteAt(int x, int y);
 
-    // Gets location of erasing the sprite
+    /**
+     * This method gets the current coordinates of where the current
+     * sprite is erased
+     * @param x - the x coordinate
+     * @param y - the y coordinate
+     */
     void erasedCurrentSpriteAt(int x, int y);
+
+    /**
+     * Goes to the next sprite
+     */
     void goToNextSprite();
+
+    /**
+     * Adds current sprite to the spritesheet
+     */
     void addSprite();
+
+    /**
+     * Removes current sprite
+     */
     void removeSprite();
 };
 #endif // SPRITESHEET_H
